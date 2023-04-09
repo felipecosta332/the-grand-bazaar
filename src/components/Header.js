c4292df638151c4d1f907867cd963be4757395bd
Felipe Costa <felipecosta776@outlook.com>
2023-04-09 17:26:22 -0300

    sets up first components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,25 @@
+import { Link, NavLink } from "react-router-dom";
+import Logo from "../assets/logo.svg";
+import "./Header.css";
+
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <Link to="/" className="logo">
+        <img src={Logo} alt="The Grand Bazaar Logo" />
+        <span>The Grand Bazaar</span>
+      </Link>
+      <nav className="navigation">
+        <NavLink to="/" className="link" end>
+          Home
+        </NavLink>
+        <NavLink to="/cart" className="link">
+          Cart
+        </NavLink>
+      </nav>
+      <Link to="/cart" className="items">
+        <span>Cart: 2</span>
+      </Link>
+    </header>
+  );
 };
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,3 +1,14 @@
-export const ProductCard = () => {
-  return <div>ProductCard</div>;
+export const ProductCard = ({ product }) => {
+  const { name, price, image } = product;
+
+  return (
+    <div className="productCard">
+      <img src={image} alt={name} />
+      <p className="name">{name}</p>
+      <div className="action">
+        <p>{price}</p>
+        <button>Add To Cart</button>
+      </div>
+    </div>
+  );
 };
