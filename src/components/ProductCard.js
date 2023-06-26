9c1557bde3e5e0db7116d6dfb8f965c8d6c34ef2
Felipe Costa <felipecosta776@outlook.com>
2023-06-26 18:23:11 -0300

    adds update method for add/remove product action

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,9 +1,16 @@
+import { useEffect, useState } from "react";
 import { useCart } from "../context/CartContext";
 import "./ProductCard.css";
 
 export const ProductCard = ({ product }) => {
-  const { addToCart } = useCart();
-  const { name, price, image } = product;
+  const { cartList, addToCart, removeFromCart } = useCart();
+  const [isInCart, setIsInCart] = useState(false);
+  const { id, name, price, image } = product;
+
+  useEffect(() => {
+    const productIsInCart = cartList.find((cartItem) => cartItem.id === id);
+    setIsInCart(productIsInCart);
+  }, [cartList, id]);
 
   return (
     <div className="productCard">
@@ -11,7 +18,11 @@ export const ProductCard = ({ product }) => {
       <p className="name">{name}</p>
       <div className="action">
         <p>${price}</p>
-        <button onClick={() => addToCart(product)}>Add To Cart</button>
+        {isInCart ? (
+          <button className="remove" onClick={() => removeFromCart(product)}>Remove</button>
+        ) : (
+          <button onClick={() => addToCart(product)}>Add To Cart</button>
+        )}
       </div>
     </div>
   );
